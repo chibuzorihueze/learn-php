93938026f7ba63cacc2ebf96b30703590a93bce4
Chibuzor Ihueze <krisykord@gmail.com>
2022-12-07 10:01:07 +0100

    Integer files were added

---
+++ src/variables/integer.php
@@ -0,0 +1,8 @@
+<?php
+
+$number1 = 1;
+$number2 = 2;
+
+$number3 = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is equal to $number3";
---
+++ src/variables/integer2.php
@@ -0,0 +1,8 @@
+<?php
+
+$number1 = 5;
+$number2 = 7;
+
+$number3 = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is equal to $number3";
