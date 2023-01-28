e7b08c6a311dcb3ea0d0aa09f69b7663ee9cc2bd
unknown <krisykord@gmail.com>
2023-01-28 11:30:22 -0800

    using sprintf function to reformat assignment3

--- src/variables/assignment/assignment3.php
+++ src/variables/assignment/assignment3.php
@@ -8,4 +8,10 @@ $time = 2; // years
 
 $intrest = $principal * $rate * $time;
 
-echo "your intrest in 2 years is $intrest Naira";
+echo sprintf(
+    'Dear customer you invested N%d at a profit rate of N%.2f your intrest for %dyears is N%d',
+    number_format($principal, 2),
+    round($rate, 2),
+    $time,
+    $intrest,
+);
