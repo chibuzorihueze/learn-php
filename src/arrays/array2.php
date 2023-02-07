6d5c81253d7da30a4aa8a7d6ad5cbe24bc79aeed
unknown <krisykord@gmail.com>
2023-02-07 11:31:02 -0800

    explaining array2 assignment

--- src/arrays/array2.php
+++ src/arrays/array2.php
@@ -6,3 +6,8 @@ $integer_array = [ 1, 7, 10, 6, 4 ];
 echo $integer_array[5] . PHP_EOL;
 
 // Can you explain your answer???
+/*
+    Accessing an invalid index returns an error.
+    An array starts from index zero (0).
+    In this case, the size of this array is 5 and the last value is on index 4, hence the error. Index 5 is not recognized in this array.
+*/
