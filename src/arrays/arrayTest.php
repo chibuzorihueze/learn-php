bba3cac014adeeacd47ea1922b4663ffd1cb41dd
unknown <krisykord@gmail.com>
2023-02-08 15:08:45 -0800

    looping through array

--- src/arrays/arrayTest.php
+++ src/arrays/arrayTest.php
@@ -4,9 +4,15 @@
 
 $int_array = [1,2,3,4,5,56,55];
 
-echo $int_array[4]. PHP_EOL;
+echo $int_array[3]. PHP_EOL;
 
 $checkLenght = count($int_array);
 
 echo $checkLenght. PHP_EOL;
 
+//looping through the array
+
+for($i=0; $i < $checkLenght; $i++){
+
+    echo $int_array[$i];
+};
