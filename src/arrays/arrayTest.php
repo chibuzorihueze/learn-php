4d4643a4c10c8ae61a711321c74bd6440dc239a8
unknown <krisykord@gmail.com>
2023-02-08 15:26:30 -0800

    testing and trying out associative arrays

--- src/arrays/arrayTest.php
+++ src/arrays/arrayTest.php
@@ -1,6 +1,6 @@
 <?php
 
-//printing array index
+//Printing array index
 
 $int_array = [1,2,3,4,5,56,55];
 
@@ -8,11 +8,19 @@ echo $int_array[3]. PHP_EOL;
 
 $checkLenght = count($int_array);
 
-echo $checkLenght. PHP_EOL;
+echo $checkLenght . PHP_EOL;
 
-//looping through the array
+//Looping through the array
 
 for($i=0; $i < $checkLenght; $i++){
 
-    echo $int_array[$i];
-};
+    echo $int_array[$i] . PHP_EOL;
+
+}; 
+
+//Associative array
+
+$name_age_array = ["34"=>"john", "pala"=>"33", "boa"=>"42"];
+echo $name_age_array["34"] . $name_age_array["pala"];
+echo "<br>";
+echo "pala is " . $name_age_array['pala']. " years old". PHP_EOL;
